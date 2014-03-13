9f1030680564aec608b24e2b0dd490fb9b9c8969
wenpin.cui <wenpincui224@gmail.com>
2014-03-13 23:11:51 +0800

    save.

--- Co-Reading/src/com/example/co_reading/FragmentTab.java
+++ Co-Reading/src/com/example/co_reading/FragmentTab.java
@@ -17,104 +17,80 @@ import com.joanzapata.pdfview.PDFView;
 import com.joanzapata.pdfview.listener.OnPageChangeListener;
 
 public class FragmentTab extends Fragment implements OnPageChangeListener {
-	
-	private static final int REQUEST_CHOOSER = 1234;
-	
-	private View m_rootView = null;
-	private String m_currentFileName = "";
-	
-	// find id by view
-	public static final String 	SAMPLE_FILE = "sample.pdf";
-	public static final String 	ABOUT_FILE = "about.pdf";
-	private PDFView m_pdfView = null;
-	
+
+    private static final int REQUEST_CHOOSER = 1234;
+
+    private View m_rootView = null;
+    private String m_currentFileName = "";
+
+    // find id by view
+    public static final String 	SAMPLE_FILE = "sample.pdf";
+    public static final String 	ABOUT_FILE = "about.pdf";
+    private PDFView m_pdfView = null;
+
     private String 			m_pdfName = SAMPLE_FILE;
     private Integer 		m_pageNumber = 1;
-    
-    private SurfaceView mSurfaceView;
-    private SurfaceHolder mSurfaceHolder;
-	
-	@Override
-	public void onCreate (Bundle savedInstanceState) {		
-		super.onCreate(savedInstanceState);
-		
-		//m_pdfView = new PDFView(getActivity(), null);
-		
-		createSurfaceView();
-		
-		Log.v("tab", "onCreate"+m_pdfView);
-		Intent getContentIntent = FileUtils.createGetContentIntent();
-		Intent intent = Intent.createChooser(getContentIntent, "Select a PDF file");
-		startActivityForResult(intent, REQUEST_CHOOSER);
-	}
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, 
-			Bundle savedInstanceState) {
 
-		Log.v("tab", "onCreateView");
-		//m_rootView = inflater.inflate(R.layout.fragment_pdfview, container, false);
-		m_rootView = m_pdfView;
-		
-		//m_pdfView = (PDFView)m_rootView.findViewById(R.id.pdfView);
-		//TextView textView = (TextView)m_rootView.findViewById(R.id.pdfViewfragment);
-		
-		return m_rootView;
-	}
-	
-	private void createSurfaceView() {
-		mSurfaceView = new SurfaceView(getActivity());
-		mSurfaceHolder = mSurfaceView.getHolder();
-		mSurfaceHolder.addCallback(new SurfaceViewCallback());
-		mSurfaceView.setVisibility(View.VISIBLE);
-		try {
-			Canvas canvas = mSurfaceHolder.lockCanvas();
-			if (canvas != null) {
-				Paint paint = new Paint();
-				paint.setStrokeWidth(2.0f);
-				paint.setColor(Color.RED);
-				canvas.drawText("hello", 50, 50, paint);
-				mSurfaceHolder.unlockCanvasAndPost(canvas);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Override
-	public void onDestroy () {
-		super.onDestroy();
-		Log.v("tab", "onDestroy");
-	}
-	
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		switch (requestCode) {
-		case REQUEST_CHOOSER:
-			if (resultCode == Activity.RESULT_OK) {
-				final Uri uri = data.getData();
-				String path = FileUtils.getPath(getActivity(), uri);
-				if (path != null && FileUtils.isLocal(path)) {
-					// open file
-					if (m_rootView != null) {
-						Log.d("open pdf", "file path:"+path);
-						// display(path, true);
-						File file = FileUtils.getFile(getActivity(), uri);
-						if (file != null) {
-							Log.d("file", "file:"+file);
-							display(file, true);
-						}
-						//TextView textView = (TextView)m_rootView.findViewById(R.id.pdfViewfragment);
-						//textView.setText(path);
-					}
-				}
-			}
-			break;
-		default:
-			break;
-		}		
-	}
-	
+    @Override
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //m_pdfView = new PDFView(getActivity(), null);
+
+        Log.v("tab", "onCreate"+m_pdfView);
+        Intent getContentIntent = FileUtils.createGetContentIntent();
+        Intent intent = Intent.createChooser(getContentIntent, "Select a PDF file");
+        startActivityForResult(intent, REQUEST_CHOOSER);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        Log.v("tab", "onCreateView");
+        //m_rootView = inflater.inflate(R.layout.fragment_pdfview, container, false);
+        m_rootView = m_pdfView;
+
+        //m_pdfView = (PDFView)m_rootView.findViewById(R.id.pdfView);
+        //TextView textView = (TextView)m_rootView.findViewById(R.id.pdfViewfragment);
+
+        return m_rootView;
+    }
+
+    @Override
+    public void onDestroy () {
+        super.onDestroy();
+        Log.v("tab", "onDestroy");
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+        case REQUEST_CHOOSER:
+            if (resultCode == Activity.RESULT_OK) {
+                final Uri uri = data.getData();
+                String path = FileUtils.getPath(getActivity(), uri);
+                if (path != null && FileUtils.isLocal(path)) {
+                    // open file
+                    if (m_rootView != null) {
+                        Log.d("open pdf", "file path:"+path);
+                        // display(path, true);
+                        File file = FileUtils.getFile(getActivity(), uri);
+                        if (file != null) {
+                            Log.d("file", "file:"+file);
+                            display(file, true);
+                        }
+                        //TextView textView = (TextView)m_rootView.findViewById(R.id.pdfViewfragment);
+                        //textView.setText(path);
+                    }
+                }
+            }
+            break;
+        default:
+            break;
+        }
+    }
+
     void afterViews() {
         display(m_pdfName, false);
     }
@@ -123,46 +99,46 @@ public class FragmentTab extends Fragment implements OnPageChangeListener {
         if (!displaying(ABOUT_FILE))
             display(ABOUT_FILE, true);
     }
-    
+
     public void setting() {
     	if (!displaying(SAMPLE_FILE))
-    		display(SAMPLE_FILE, true);    	
+            display(SAMPLE_FILE, true);
     }
 
     private void display(String assetFileName, boolean jumpToFirstPage) {
         if (jumpToFirstPage) m_pageNumber = 1;
         // setTitle(m_pdfName = assetFileName);
-        
+
         m_pdfView.fromAsset(assetFileName)
-                .defaultPage(m_pageNumber)
-                .onPageChange(this)
-                .load();
-    }
-    
-    private void display(File file, boolean jumpToFirstPage) {
-    	if (jumpToFirstPage) m_pageNumber = 1;
-    	
-    	m_pdfView.fromFile(file)
-    		.defaultPage(m_pageNumber)
-    		.onPageChange(this)
-    		.load();    	
+            .defaultPage(m_pageNumber)
+            .onPageChange(this)
+            .load();
     }
 
-	@Override
-	public void onPageChanged(int page, int pageCount) {
-		// TODO Auto-generated method stub
-		m_pageNumber = page;
-	}
-	
-//    @Override
-//    public void onBackPressed() {
-//        if (ABOUT_FILE.equals(m_pdfName)) {
-//            display(SAMPLE_FILE, true);
-//        } else {
-//            super.onBackPressed();
-//        }
-//    }
-    
+    private void display(File file, boolean jumpToFirstPage) {
+    	if (jumpToFirstPage) m_pageNumber = 1;
+
+    	m_pdfView.fromFile(file)
+            .defaultPage(m_pageNumber)
+            .onPageChange(this)
+            .load();
+    }
+
+    @Override
+    public void onPageChanged(int page, int pageCount) {
+        // TODO Auto-generated method stub
+        m_pageNumber = page;
+    }
+
+    //    @Override
+    //    public void onBackPressed() {
+    //        if (ABOUT_FILE.equals(m_pdfName)) {
+    //            display(SAMPLE_FILE, true);
+    //        } else {
+    //            super.onBackPressed();
+    //        }
+    //    }
+
     private boolean displaying(String fileName) {
         return fileName.equals(m_pdfName);
     }
--- Co-Reading/src/com/example/co_reading/TabListener.java
+++ Co-Reading/src/com/example/co_reading/TabListener.java
@@ -5,41 +5,43 @@ import android.app.ActionBar.Tab;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.util.Log;
+import android.view.*;
+import android.graphics.*;
 
 public class TabListener implements ActionBar.TabListener {
 
-	private Fragment m_fragment = null;
-	private boolean m_isAdded = false;
+    private Fragment m_fragment = null;
+    private boolean m_isAdded = false;
 
-	public TabListener(Fragment fragment) {
-		Log.v("tablistener", "Tablistener constructor");
-		this.m_fragment = fragment;
-	}
+    public TabListener(Fragment fragment) {
+        Log.v("tablistener", "Tablistener constructor");
+        this.m_fragment = fragment;
+    }
 
-	@Override
-	public void onTabSelected(Tab tab, FragmentTransaction ft) {
-		Log.v("TabListener", "onTabSelected");
-		// ft.replace(R.id.fragment_container, m_fragment);
-		if (m_isAdded == false) {
-			if (m_fragment != null) {
-				ft.add(R.id.fragment_container, m_fragment);
-				m_isAdded = true;
-			}
-		} else {
-			ft.show(m_fragment);
-		}
-	}
-	
-	@Override
-	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-		Log.v("TabListener", "onTabUnselected:"+m_fragment);
-		ft.hide(m_fragment);
-		//ft.remove(fragment);
-	}
-	
-	@Override
-	public void onTabReselected(Tab tab, FragmentTransaction ft) {
-		Log.v("TabListener", "onTabReselected");
-		ft.show(m_fragment);
-	}
+    @Override
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        Log.v("TabListener", "onTabSelected");
+        // ft.replace(R.id.fragment_container, m_fragment);
+        if (m_isAdded == false) {
+            if (m_fragment != null) {
+                ft.add(R.id.fragment_container, m_fragment);
+                m_isAdded = true;
+            }
+        } else {
+            ft.show(m_fragment);
+        }
+    }
+
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+        Log.v("TabListener", "onTabUnselected:"+m_fragment);
+        ft.hide(m_fragment);
+        //ft.remove(fragment);
+    }
+
+    @Override
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+        Log.v("TabListener", "onTabReselected");
+        ft.show(m_fragment);
+    }
 }
